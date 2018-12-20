9b33850fea2a16d5b81987d8aa8958f64d9659d0
Edison dos Santos <38053585+Edison2ST@users.noreply.github.com>
2018-12-20 15:19:59 -0400

    v. 1.2.0

--- README.md
+++ README.md
@@ -1,17 +1,20 @@
 # EQN
 
-This project was called EQN in reference to the ability of the program to solve systems of equations nxn, but it is now possible to solve any Gauss-Jordan elimination (not necessarily nxn) with this program. It is available in javascript at [https://edison2st.github.io/EQN/](https://edison2st.github.io/EQN/) with a limit of 420 rows with columns. If you want to solve larger matrices, you could [download the program in c++ here (Windows only)](https://github.com/Edison2ST/EQN/releases/download/v1.1.0/EQN.exe).
+This project was called EQN in reference to the ability of the program to solve systems of equations nxn, but it is now possible to solve any Gauss-Jordan elimination (not necessarily nxn) with this program. It is available in C++ and also in javascript at [https://edison2st.github.io/EQN/](https://edison2st.github.io/EQN/).
 
 # How to use
 
 Javascript (website):
 
-- Add the number of rows and columns that you want to use (Ex: 2 rows and 3 columns), it must be less or equal than 420 rows with columns, then press "Send".
+- Add the number of rows and columns that you want to use (Ex: 2 rows and 3 columns), then press "Send".
 - x1-1 means first row and first column, x1-2 means first row and second column, x1-n means first row and nth column, x2-1 means second row and first column, xn-1 means nth row and first column, xn-n means nth row and nth column. Fill all those entries with your values. Negative numbers are valid. **NOTE: Put 0.25 instead of 1/4. This program does not work with fractions of numbers.**
 - Press "Send" and take a look to the answer and the step-by-step of the program!
 
-C++ (Windows application):
+C++ (source code):
 
+This version is designed to have a good performance, therefore some features are not available
+
+- Download the [source code](https://github.com/Edison2ST/EQN/archive/v1.2.0.zip), compile it and run the program.
 - Add the number of rows that you want to use, press "ENTER", and then add the number of columns that you want to use, press "ENTER".
 - x1-1 means first row and first column, x1-2 means first row and second column, x1-n means first row and nth column, x2-1 means second row and first column, xn-1 means nth row and first column, xn-n means nth row and nth column. Fill all those entries with your values, and press "ENTER" for each value. Negative numbers are valid. **NOTE: Put 0.25 instead of 1/4. This program does not work with fractions of numbers.**
 - Take a look to the answer and the step-by-step of the program!
--- main.cpp
+++ main.cpp
@@ -5,10 +5,11 @@ using namespace std;
 int main(int argc, char *argv[])
 {
     int row, column;
-    cout << "EQN v. 1.1.0" << endl << "Size of matrix" << endl << "Rows: ";
+    cout << "EQN v. 1.2.0" << endl << "Size of matrix" << endl << "Rows: ";
     cin >> row;
     cout << "Columns: ";
     cin >> column;
+    // First, put all the values in the variable "matrix"
     double matrix[row][column];
     for(int i = 0; i < row; i++)
     {
@@ -18,47 +19,38 @@ int main(int argc, char *argv[])
             cin >> matrix[i][j];
         }
     }
+    // Start calculating
     for (int i = 0; i < row; i++)
     {
-        double use = matrix[i][i];
-        if (use == 0)
+        double use = matrix[i][i]; // Get the position of the matrix at the same number of row and column, ex: x1-1, x2-2, x3-3, etc
+        if (use == 0) // If use is equal to 0, try to exchange rows
         {
             int j;
-            for (j = i+1; j < row; j++)
+            for (j = i+1; j < row; j++) // Start at the next row trying to exchange rows. It will perform some operations later, so it will check only next rows and not previous rows
             {
-                if (matrix[j][i] != 0)
+                if (matrix[j][i] != 0) // If true, then it is possible to exchange rows
                 {
                     double oldmatrix[column], newmatrix[column];
+                    for (int k = 0; k < column; k++) oldmatrix[k] = matrix[i][k];
                     for (int k = 0; k < column; k++)
                     {
-                        oldmatrix[k] = matrix[i][k];
-                        newmatrix[k] = matrix[j][k];
-                    }
-                    for (int k = 0; k < column; k++)
-                    {
-                        matrix[i][k] = newmatrix[k];
+                        matrix[i][k] = matrix[j][k];
                         matrix[j][k] = oldmatrix[k];
                     }
+                    use = matrix[i][i];
                     cout << endl << "Exchanging rows..." << endl << endl;
-                    for (int k = 0; k < column; k++)
-                    {
-                        cout << "x" << i+1 << "-" << k+1 << " = " << matrix[i][k] << endl;
-                    }
-                    for (int k = 0; k < column; k++)
-                    {
-                        cout << "x" << j+1 << "-" << k+1 << " = " << matrix[j][k] << endl;
-                    }
+                    for (int k = 0; k < column; k++) cout << "x" << i+1 << "-" << k+1 << " = " << matrix[i][k] << endl;
+                    for (int k = 0; k < column; k++) cout << "x" << j+1 << "-" << k+1 << " = " << matrix[j][k] << endl;
                     break;
                 }
             }
-            if (j == row)
+            if (j == row) // If true, then it is not possible to exchange rows
             {
-                cout << endl << "0's, half of the Gauss-Jordan" << endl;
-                break;
+                cout << endl << "Zeros, half of the Gauss-Jordan" << endl;
+                break; // And stop calculating, because it could alter the answer
             }
         }
-        use = matrix[i][i];
-        if (use != 1)
+        if (use != 1) // If true, divide all the row by use, so use will be 1 too
         {
             cout << endl << "x" << i+1 << "-" << i+1 << " is not 1, divide it by " << use << ". Result:" << endl;
             for (int j = 0; j < column; j++)
@@ -68,10 +60,10 @@ int main(int argc, char *argv[])
                 cout << "x" << i+1 << "-" << j+1 << " / " << use << " = " << matrix[i][j] << endl;
             }
         }
-        for (int j = i+1; j < row; j++)
+        for (int j = i+1; j < row; j++) // Check the values in the same column, but in the next rows. We need them to be 0
         {
             double cons = matrix[j][i];
-            if (cons != 0)
+            if (cons != 0) // If true, perform operations to be 0
             {
                 cout << endl << "x" << j+1 << "-" << i+1 << " is not 0, do x" << j+1 << "-k = -x" << j+1 << "-" << i+1 << "*x" << i+1 << "-k+x" << j+1 << "-k, where k is every column of x" << j+1 << ". Result:" << endl;
                 for (int k = i; k < column; k++)
@@ -82,16 +74,14 @@ int main(int argc, char *argv[])
             }
         }
     }
+    // We have finished the first half, we need some values in the second half to be 0
     for (int i = row-1; i >= 0; i--)
     {
-        if (matrix[i][i] == 0)
-        {
-            break;
-        }
+        if (matrix[i][i] == 0) break; // Because it could alter the answer
         for (int j = i-1; j >= 0; j--)
         {
             double cons = matrix[j][i];
-            if (cons != 0)
+            if (cons != 0) // Perform operations to be 0
             {
                 cout << endl << "x" << j+1 << "-" << i+1 << " is not 0, do x" << j+1 << "-k = -x" << j+1 << "-" << i+1 << "*x" << i+1 << "-k+x" << j+1 << "-k, where k is every column of x" << j+1 << ". Result:" << endl;
                 for (int k = i; k < column; k++)
@@ -102,14 +92,9 @@ int main(int argc, char *argv[])
             }
         }
     }
+    // Done, now print the answer on the screen
     cout << endl << "Answer:" << endl;
-    for (int i = 0; i < row; i++)
-    {
-        for (int j = 0; j < column; j++)
-        {
-            cout << "x" << i+1 << "-" << j+1 << " = " << matrix[i][j] << endl;
-        }
-    }
+    for (int i = 0; i < row; i++) for (int j = 0; j < column; j++) cout << "x" << i+1 << "-" << j+1 << " = " << matrix[i][j] << endl;
     cout << "Press ENTER key to exit." << endl;
     cin.ignore();
     cin.get();
